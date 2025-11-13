18e3ab9e0060e508e81c20dfe55686d62d41ac75
grover <groverchoquevillca80@gmail.com>
2025-11-13 04:12:29 -0400

    gnrok

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [
+      'localhost',
+      '.ngrok-free.dev',
+      '.ngrok.io',
+      'commemoratory-dorotha-doubtingly.ngrok-free.dev'
+    ]
+  }
 })
