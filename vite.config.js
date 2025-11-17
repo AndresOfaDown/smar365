4eaa8183f261dfb3740e6011b3bff5063c71053f
AndrésOfaDown <leonardo61333@gmail.com>
2025-11-17 17:44:53 -0400

    ultimo fron

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: [
-      'localhost',
-      '.ngrok-free.dev',
-      '.ngrok.io',
-      'commemoratory-dorotha-doubtingly.ngrok-free.dev'
-    ]
-  }
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
